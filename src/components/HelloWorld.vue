bbe414ce24d6f306da6c048dcea2dfdc9036bc46
vimalrajan242001 <vimalrajansret@gmail.com>
2020-11-12 19:00:57 +0530

    project file

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,63 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="hello container">
+              <form @submit.prevent="submitForm">
+
+  <div class="col-md-11 mb-3">
+    <label >Nested A</label>
+    <input  class="form-control "  v-model.trim="$v.profile.name.$model"/>
+  <div class="invalid-feedback" v-if="!$v.profile.name.required && $v.profile.name.$dirty" >Field is required.</div>
+  </div>
+  
+  <div class="col-md-11 mb-3">
+    <label class="form__label">Nested B</label>
+    <input class="form-control"  v-model.trim="$v.profile.address.$model"/>
+  <div class="text-danger" v-if="!$v.profile.address.required && $v.profile.address.$dirty" >Field is required.</div>
+  </div>
+  <div class="form-group" :class="{ 'form-group--error': $v.profile.$error }"></div>
+  <div class="error" v-if="$v.profile.$error">Form is invalid.</div>
+<button type="submit"  class="btn btn-primary mt-2" >submit</button>
+          </form>
+
   </div>
 </template>
 
 <script>
+import {
+  required,
+} from "vuelidate/lib/validators";
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+ data() {
+    return {
+      profile: {
+        name: '',
+        address: ''
+      }
+    }
+  },
+  validations: {
+    profile: {
+    name: {
+        required
+      },
+      address: {
+        required
+      }
+    }
+  },
+  methods: {
+       submitForm() {
+      this.$v.$touch();
+
+      if (!this.$v.$invalid) {
+      //logic
+      // this.setDone('second', 'third')
+console.log("working")
+}else{
+  console.log("not")
+}
+      }
+  },
 }
 </script>
 
